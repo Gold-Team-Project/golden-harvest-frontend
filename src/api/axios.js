70b3a588b5e0e5cae008e274f468cc4539b42c85
KyujinPark01 <rbwls1100@naver.com>
2026-01-17 22:49:26 +0900

    feat: user-inquiry 페이지 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'http://localhost:8088/api',
     timeout: 5000,
 })
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
+import UserDefaultLayout from "@/layouts/user/UserDefaultLayout.vue";
+import InquiryListView from "@/views/inquiry/InquiryListView.vue";
+import InquiryCreateView from "@/views/inquiry/InquiryCreateView.vue";
 
 
+let DashboardView;
 const routes = [
+    {
+        path: '/',
+        component: UserDefaultLayout,
+        children: [
+            {
+                path: 'inquiries',
+                name: "inquiryList",
+                component: InquiryListView,
+                meta: { title: '설정 / 문의 내역'}
+            },
+            {
+                path: 'inquiries/create',
+                name: "inquiryCreate",
+                component: InquiryCreateView,
+                meta: { title: '설정 / 문의 내역 / 문의 등록 '}
+            },
+        ],
+    },
 ]
 
 export default createRouter({
