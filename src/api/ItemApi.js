e74fb26e32f3494c9e43310fe829144d0a0014be
KyujinPark01 <rbwls1100@naver.com>
2026-01-29 09:48:41 +0900

    refactor: api

--- src/api/ItemApi.js
+++ src/api/ItemApi.js
@@ -9,27 +9,29 @@ export async function getAllItems(filters) {
             startDate: filters.startDate || null,
             endDate: filters.endDate || null,
         };
-        const response = await http.get('/admin/item', { params });
+        const response = await http.get('/items', { params });
         return response.data;
     } catch (error) {
         console.error('Error fetching all items:', error);
         throw error;
     }
 }
-
 export async function getLots(filters) {
     try {
         const params = {
-            page: filters.page,
-            size: filters.size,
-            lotNo: filters.lotNo || null,
-            itemName: filters.itemName || null,
-            status: filters.status || null,
+            skuNo: filters.skuNo,
+            startDate: filters.startDate,
+            endDate: filters.endDate,
+            limit: filters.size,
+            offset: (filters.page - 1) * filters.size,
         };
-        const response = await http.get('/admin/item', { params });
+
+        const response = await http.get('/admin/items', { params });''
+        console.log(response.data)
         return response.data;
     } catch (error) {
         console.error('Error fetching lots:', error);
         throw error;
     }
 }
+
